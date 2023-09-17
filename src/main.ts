5c42f2977e289568ac7f3e99fa1f17bdf649205e
Thassia <thassiadev@gmail.com>
2023-09-17 08:16:04 +0200

    chore: start
    Global CSS config | install fonts and icons lib.

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import './main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faRotateRight } from '@fortawesome/free-solid-svg-icons'
+library.add(faRotateRight)
+
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
